69215f31693f0940a89ff07644b738e290faf8f3
DmitriyKaplan28 <dsk007@mail.ru>
2023-04-04 20:52:00 +0300

    angular practise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core'
 
-interface IUser {
-  age: number
+interface Fruit {
+  id: string
   name: string
+  price: number
 }
 
 @Component({
@@ -11,10 +12,15 @@ interface IUser {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  appTitle = 'instagram'
-
-  user: IUser = {
-    age: 10,
-    name: 'userName',
-  }
+  fruits: Fruit[] = [
+    { id: '1', name: 'apple', price: 10 },
+    { id: '2', name: 'orange', price: 20 },
+    { id: '3', name: 'watermelon', price: 30 },
+    { id: '4', name: 'banana', price: 5 },
+    { id: '5', name: 'pears', price: 12 },
+    { id: '6', name: 'raspberries', price: 18 },
+    { id: '7', name: 'avocados', price: 14 },
+    { id: '8', name: 'mangoes', price: 3 },
+    { id: '9', name: 'kiwifruit', price: 7 },
+  ]
 }
